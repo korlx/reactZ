ff673e59eaee047cb6994b9b1581d34f12f9e059
Konstantin Piterman <116492825+korlx@users.noreply.github.com>
2022-12-12 11:26:25 +0000

    asdasd

--- new-app/src/components/home.jsx
+++ new-app/src/components/home.jsx
@@ -1,6 +1,40 @@
 import React from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
 
 const Home = () => {
+    const[data,setDate]=useState({})
+    const doApi=async ()=>{
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=tel%20aviv&appid=3f55a24e37304e63b483d73891af88ba`
+    const{data}=await axios(url)
+    const obj={
+        location:{
+            city:data.name,
+            country:data.sys.country,
+           sunset:data.sys.sunset
+        },
+        weather:{
+            temp:data.main.temp,
+            humidity:data.main.humidity,
+            wind:data.wind.speed,
+            desc:data.weather.description
+        },
+        coord: {
+            lon:data.coord.lon,
+            lat:data.coord.lat
+            }
+        
+    }
+    console.log(obj);
+    }
+
+    useEffect(()=>{
+        doApi();
+    },[])
+    
+    
     return (
         <div><h1>Home site</h1></div>
     )
--- new-app/src/routers/appRouters.jsx
+++ new-app/src/routers/appRouters.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Layout from '../layout/layoutUser'
+
 import Home from '../components/home'
-import Page2 from '../Pages/Page2'
-import Page1 from '../Pages/Page1'
-import Page3 from '../Pages/Page3'
+
 
 const AppRouters = () => {
     return (
         <Router>
             <Routes>
-                {/* <Route path='/' element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path='/Page1' element={<Page1 />} />
-                    <Route path='/Page2' element={<Page2 />} />
-                    <Route path='/Page3' element={<Page3 />} />
-                </Route> */}
+            <Route index element={<Home />} />
 
                 <Route path='/*' element={<h1 className='d-flex justify-content-center  '>Not found,fuck you </h1>} />
             </Routes>
