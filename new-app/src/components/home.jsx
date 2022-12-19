0eee0a26092fb39e71b500bc5db42ebc7127b9bb
Konstantin Piterman <116492825+korlx@users.noreply.github.com>
2022-12-19 12:08:44 +0000

    v2

--- new-app/src/components/home.jsx
+++ new-app/src/components/home.jsx
@@ -2,18 +2,26 @@ import React, { useState } from 'react'
 
 
 const Home = () => {
-
+  const [ar, setAr] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
   return (
     <div >
-      <div >
+      <div>
         <h1 className="text-3xl font-bold underline text-center">
           TailWindCss
         </h1>
       </div>
-      <div className='flex flex-row px-[100px]'>
-        <div className='bg-blue-900 text-white h-[100px] w-[100px]'>
-          1
-        </div>
+      <div className='flex flex-row flex-wrap justify-between md:p-[50px] p-[20px]'>
+
+        {ar.map(item => (
+          <>
+            {item < 10 &&
+              <div className='flex justify-center items-center shadow-lg border bg-blue-900 text-white md:mt-[1%] mt-[4%] h-[300px] lg:basis-[24.2%] md:basis-[32.5%] basis-full rounded-[20px] '>
+                <p className='font-bold text-[100px]'>{item}</p>
+              </div>}
+          </>
+
+        ))}
+
       </div>
 
     </div>
